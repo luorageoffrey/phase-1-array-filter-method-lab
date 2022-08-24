3a3547f8d7f72e4ab8cea93190a6fe594ca70a78
luorageoffrey <geoffrey.luora@student.moringaschool.com>
2022-08-24 20:52:04 +0300

    done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution here
+
+function findMatching(drivers,name){
+
+    return drivers.filter(driver => {
+
+        if (driver.localeCompare(name,undefined,  {sensitivity: 'base'}) ===0){
+            return true
+        }else {
+            return false
+        }
+
+        //driver.localeCompare(name, {sensitivity: 'base'})
+
+    })
+    
+}
+
+function fuzzyMatch(drivers,name){
+
+    return drivers.filter(driver => driver.startsWith(name))
+
+}
+
+function matchName(drivers,name){
+
+    return drivers.filter(driver => driver.name.match(name))
+
+}
+
